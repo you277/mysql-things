3852b72de86a5cbf9db5f7c5a2d6ce4322df31e3
you <144391848+you277@users.noreply.github.com>
2025-05-14 14:38:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(user='',
-                                         password='',
-                                         host='10.8.37.226',
-                                         database='')
+                                   password='',
+                                   host='10.8.37.226',
+                                   database='')
 
 
 def sql_exec(connection, cmd):
@@ -24,18 +24,62 @@ def get_student_schedule(student_id):
     return sql_exec(connection, f"CALL get_student_schedule({student_id})")
 
 
-connection = get_connection()
+def get_teacher_schedule(teacher_id):
+    return sql_exec(connection, f"CALL get_teacher_schedule({teacher_id})")
 
-student_id = input("Enter student ID: ")
-schedule = get_student_schedule(student_id)
 
-pd = 0
-for data in schedule:
-    pd += 1
+def get_query():
+    print("Are you a student or a teacher?")
+    print("1. Student")
+    print("2. Teacher")
+
+    is_student = input("> ") == "1"
+    user_type = "Student" if is_student else "Teacher"
+
     print()
-    print(f"Period: {pd}")
-    print(f"Course: {data[0]}")
-    print(f"Room: {data[1]}")
-    print(f"Teacher: {data[2]} {data[3]}")
+    user_id = input(f"{user_type} ID: ")
+    return is_student, user_id
+
+
+def display_schedule(schedule, pd_display):
+    periods = {}
+    for pd in schedule:
+        periods[pd[4]] = pd
+    for i in range(10):
+        period_num = i + 1
+        pd_display(period_num, periods[period_num] if period_num in periods else None)
+
+
+def display_student_schedule(pd_num, period):
+    print()
+    print(f"Period: {pd_num}")
+    if period:
+        print(f"Course: {period[0]}")
+        print(f"Room: {period[1]}")
+        print(f"Teacher: {period[2]} {period[3]}")
+    else:
+        print("(None)")
+
+
+def display_teacher_schedule(pd_num, period):
+    print()
+    print(f"Period: {pd_num}")
+    if period:
+        print(f"Course: {period[0]}")
+        print(f"Room: {period[1]}")
+    else:
+        print("(None)")
+
+
+connection = get_connection()
+is_student, user_id = get_query()
+
+if is_student:
+    schedule = get_student_schedule(user_id)
+    display_schedule(schedule, display_student_schedule)
+else:
+    schedule = get_teacher_schedule(user_id)
+    display_schedule(schedule, display_teacher_schedule)
+
 
 connection.close()
